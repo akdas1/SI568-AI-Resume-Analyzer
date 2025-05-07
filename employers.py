862cf12df2f46d3853a3017e93a9014bd8c4a8a0
Amit Das <112508515+akdas1@users.noreply.github.com>
2025-05-07 03:11:31 -0400

    Update employers.py

--- employers.py
+++ employers.py
@@ -5,9 +5,9 @@ from yourkey import key
 from PyPDF2 import PdfReader
 openai.api_key = key # go to yourkey.py and enter your key
 
-## we are just choosing pdf format
 model_engine = "gpt-3.5-turbo" # changed from text-davinci-002 due to depreciation
 
+## we are just choosing the PDF format for simplicity
 def get_resume(filepath):
     '''
     Takes in a filepath and returns the text of the resume
@@ -24,13 +24,13 @@ def get_resume(filepath):
     while True:
         ## check if the file exists
         if os.path.isfile(filepath) == True:
-            ## check if the file is a pdf
+            ## check if the file is a PDF
             if filepath.endswith('.pdf'):
                 reader = PdfReader(filepath)
                 resume = reader.pages[0].extract_text()
-            else: ## if the file is not a pdf
+            else: ## if the file is not a PDF
                 filepath = input('Enter the file as a pdf: ')
-        ## user can choose to exit at any point in this loop
+        ## users can choose to exit at any point in this loop
         elif filepath.lower() == 'exit':
                     print('Goodbye!')
                     quit()
@@ -55,21 +55,23 @@ def ask_question(question, history):
         answer: str
         response from the AI
     '''
-    while True:
-        # exits if chosen
-        if question.lower() == 'exit':
-            print('Goodbye!')
-            quit()
-        else: # stores question, answer, and history
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages= history + [
-            {"role": "user", "content": question}])
-        answer = response['choices'][0]['message']['content']
-        return answer
+ 
+    # exits if chosen
+    if question.lower() == 'exit':
+        print('Goodbye!')
+        quit() # will immediately quit and ignore the rest
+    # stores question, answer, and history
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = history + [{"role": "user", "content": question}])
+
+    # gets the response from the AI
+    answer = response['choices'][0]['message']['content']
+    return answer
+
 def main():
     '''
-    Runs the program then stores question and answers.
+    Runs the program, then stores questions and answers.
     Initially returns some summary based on the
     resume. Then, gave the option to input additional.
 
